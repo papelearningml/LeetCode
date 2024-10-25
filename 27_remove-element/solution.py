6f5f2a97171be8fbca58930e702d8e1a5191f893
papelearningml <babacardiouf17@live.Fr>
2024-10-25 14:16:13 +0200

    Update solution for problem 27: Remove Element

--- 27_remove-element/solution.py
+++ 27_remove-element/solution.py
@@ -1,18 +1,9 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        p1 = 0
-        p=len(nums)-1
-        b=0
-        k=0
-        while p1 < len(nums):
-            if nums[p1] == val:
-                #nums[p]=0
-                p-=1
-                p1+=1
-            else:
-                nums[b]=nums[p1]
-                b+=1
-                p1+=1
-                k+=1
+        k = 0  
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]  
+                k += 1 
 
         return k
